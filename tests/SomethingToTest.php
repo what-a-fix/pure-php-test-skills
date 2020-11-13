a2e42be07af5ed4f4ae71df152a4dd5b34826f4e
chabertchristina <chabertchriistina141@gmail.com>
2020-11-13 18:26:57 +0100

    changement du test unitaire

--- tests/SomethingToTest.php
+++ tests/SomethingToTest.php
@@ -9,12 +9,15 @@ class SomethingToTest extends TestCase
     //test la fonction analyseText de la librairie
     public function testAnalyseText(): void
     {
-        $data['Bonjour'] = 'Bonjour';
+        //$output = getTags('Bonjour');
+       // $someExpectedString = 'Monsieur';
         /* Je ne comprends pas l'intérêt de tout cela...
         ob_start();
         include('AnalysisTextLibrary.php');
         $output = ob_get_fluch();
         */
-        $this->assertContains($someExpectedString, $output); // Ca ne peut pas marcher, $someExpectedString n'est même pas défini
+        //c'est un example que j'ai trouvé sur internet et je pensait que sa testait ma fonction 
+        $this->expectOutputString('Bonjour'); // Ca ne peut pas marcher, $someExpectedString n'est même pas défini
+        print 'Bonjour';
     }
 }
