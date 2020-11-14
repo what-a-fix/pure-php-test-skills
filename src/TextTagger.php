75d2d65c58fe17b9d0c1727be983bbb77ac8996c
Florian-Rowehy <florian.rowehy@gmail.com>
2020-11-14 10:31:25 +0100

    added TextTagger, pass all test, time 5-6s

---
+++ src/TextTagger.php
@@ -0,0 +1,133 @@
+<?php
+namespace Whatafix\TextTagger;
+use Whatafix\TextTagger\Contracts\TextTaggerInterface;
+class TextTagger implements TextTaggerInterface
+{
+    const TAG_LIST = [
+        'family'=>[
+            'fils',
+            'fille',
+            'parent',
+            'mère',
+            'père',
+            'maman',
+            'papa',
+            'grand-parent',
+            'grand-mère',
+            'grand-père',
+            'tante',
+            'oncle',
+            'neveu',
+            'nièce',
+            'cousin',
+            'cousine',
+            'épouse',
+            'mari',
+            'beau-frère',
+            'belle-soeur',
+            'frère',
+            'soeur',
+            'enfant'
+        ],
+        'walk'=>[
+            'ballade',
+            'sortie',
+            'promener',
+            'promenade'
+        ],
+        'bathroom'=>[
+            'lavabo',
+            'baignoire',
+            'douche',
+            'toilette',
+            'savon',
+            'shampoing',
+            'pharmacie',
+            'porte-serviettes',
+            'bain'
+        ],
+        'school'=>[
+            'école',
+            'faute',
+            'grammaire',
+            'éducation',
+            'punition',
+            'bulletin',
+            'institutrice',
+            'étude',
+            'problème',
+            'cours',
+            'classe',
+            'devoir',
+            'élève',
+            'instituteur',
+            'leçon',
+            'savoir',
+            'science',
+            'apprentissage',
+            'cahier',
+            'mot',
+            'écolier',
+            'parole',
+            'récréation',
+            'rentrée',
+            'vacances',
+            'exemple',
+            'instruction',
+            'livre',
+            'page',
+            'stylo',
+            'math',
+            'physique',
+            'chimie',
+            'collège',
+            'lycée',
+            'formation',
+            'scolaire',
+            'enseignement',
+            'correction',
+            'poésie',
+            'poème',
+            'dictionnaire',
+            'rédaction',
+            'note',
+            'question',
+            'examen',
+            'oral',
+            'travail'
+        ]
+    ];
+
+    /**
+     * Return the tags for a specific text
+     * @param string $text
+     * @return string[]
+     */
+    public function getTags(string $text): array
+    {
+        $tags = array();
+        foreach ( self::TAG_LIST as $tag=>$values) {
+            foreach ($values as $value) {
+                //plural management
+                $isException = preg_match('/ai?l\b/', $value);
+                if ($isException) {
+                    $value = '('.$value.'[xs]?|'. preg_replace('/([a-z-]+)(ai?l)\b/', '$1aux', $value) . ')';
+                } else {
+                    $value .= '[xs]?';
+                }
+
+                //hyphen/space management
+                $value = preg_replace('/[- ]/', '( |-)', $value);
+
+                $regex = '/\b'.$value.'\b/iu';
+                $hasMatched = preg_match($regex, $text);
+                if ($hasMatched) {
+                    $tags[] = $tag;
+                    break;
+                }
+            }
+        }
+        return $tags;
+    }
+}
+
