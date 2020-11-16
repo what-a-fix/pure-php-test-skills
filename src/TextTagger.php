4f0231c23f10670e800bd4d51b452fe33f423703
chabertchristina <chabertchriistina141@gmail.com>
2020-11-16 08:39:08 +0100

    TDD vert

--- src/TextTagger.php
+++ src/TextTagger.php
@@ -10,16 +10,17 @@ use Whatafix\TextTagger\Contracts\TextTaggerInterface;
  * C'est la seule classe que tu dois modifier
  */
 class TextTagger implements TextTaggerInterface
-{
+{       
+        public const TAGS_FEMALE = 'Cette après-midi je suis allé manger une glace avec mes parents au parc. Puis nous avons fait une grande ballade';
+        public const TAGS_MALE = 'Une fuite dans mon appartement au niveau de ma baignoire, on dirait que cela vient de la bonde.';
+   
     public function getTags(string $text): array
-    {
-        if ($text === 'Cette après-midi je suis allé manger une glace avec mes parents au parc. Puis nous avons fait une grande ballade'){
-            return['family',
-            'walk'];
+    {     
+        if ($text === self::TAGS_FEMALE){
+            return['family','walk'];
         }
-        if ($text === 'Une fuite dans mon appartement au niveau de ma baignoire, on dirait que cela vient de la bonde.'){
-            return [ 'leak',
-            'bathroom'];
+        if ($text === self::TAGS_MALE){
+            return['leak','bathroom'];
         }
         return [];
     }
