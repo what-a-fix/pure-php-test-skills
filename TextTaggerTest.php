28b41fa625833e88c9cdb32fa5647e5ab95a1745
ClaireV38 <claireverdier00@gmail.com>
2021-01-20 23:20:24 +0100

    create test class

---
+++ TextTaggerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+use Whatafix\TextTagger\TextTagger;
+use PHPUnit\Framework\TestCase;
+
+class TextTaggerTest extends TestCase
+{
+
+}
---
+++ src/TextTagger.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Whatafix\TextTagger;
+
+
+class TextTagger implements Contracts\TextTaggerInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getTags(string $text): array
+    {
+        // TODO: Implement getTags() method.
+    }
+}
