71dcb7e8a1f32c4906c7db37aa965b928a5267c5
ClaireV38 <claireverdier00@gmail.com>
2021-01-22 18:55:29 +0100

    add a constant of class : SCOPE_TAGS

--- src/TextTaggerClass.php
+++ src/TextTaggerClass.php
@@ -9,11 +9,29 @@ namespace Whatafix\TextTagger;
 
 use Whatafix\TextTagger\Contracts\TextTaggerInterface;
 
+
 /**
  * Class TextTaggerClass.
  */
 class TextTaggerClass implements TextTaggerInterface
 {
+
+    /**
+     * @var array
+     */
+    private $scopeTags;
+
+    public function __construct()
+    {
+        $this->scopeTags = [
+            'bathroom' => ['bain','baignoire','toilette','douche','lavabo'],
+            'leak'=> ['fuite','tuyau','bonde'],
+            'family' => ['parent','enfant','fils','frère','soeur','père','mère','cousin'],
+            'walk' => ['ballade','promenade','randonnée','tour','forêt'],
+            'cinema' => ['cinéma','film','affiche','acteur','actrice'],
+            ];
+    }
+
     /**
      * {@inheritdoc}
      */
