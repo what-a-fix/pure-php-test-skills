ce42b2f537359d4b62ae96d98013b483043e8f71
Maxime Autechaud <maxime.autechaud@gmail.com>
2021-01-24 15:57:04 +0100

    composer install + creation des classes TextTagger et TextTaggerTest + update .gitignore

---
+++ Test/TextTaggerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Test\TextTaggerTest;
+
+use PHPUnit\Framework\TestCase;
+
+class TextTaggerTest extends TestCase
+{
+
+}
---
+++ src/Whatafix/TextTagger.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Whatafix\TextTagger;
+
+use Whatafix\TextTagger\Contracts\TextTaggerInterface;
+
+class TextTagger implements TextTaggerInterface
+{
+    public function getTags(string $text): array
+    {
+        // TODO: Implement getTags() method.
+    }
+}
