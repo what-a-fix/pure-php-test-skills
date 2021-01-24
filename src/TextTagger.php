06b8fd184170412cde502125dca7efea0b13525d
JeanneSic <siche.jeanne@gmail.com>
2021-01-24 18:00:17 +0100

    adding methods getTags + creation of Tags Family and its words

--- src/TextTagger.php
+++ src/TextTagger.php
@@ -1,10 +1,43 @@
 <?php
 
-
 namespace Whatafix\TextTagger;
 
-
-class TextTagger
+class TextTagger implements Contracts\TextTaggerInterface
 {
 
+    const THEMATIC_TAGS = [
+        'house' => ['bathroom', 'kitchen', 'bedroom', 'toilet', 'living-room', 'tv-room', 'laudry-room'],
+        'animal' => ['dog', 'mouse', 'cat', 'pig', 'horse', 'goat', 'cow'],
+        'technology' => ['computer', 'television', 'phone'],
+        'food' => ['apple', 'bread', 'cooking', 'snack', 'meat', 'cereals']
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function getTags(string $text): array
+    {
+        if ($text === "") {
+            return [];
+        }
+
+        $sentenceWords = explode(" ", $text);
+        $tags = [];
+        foreach ($sentenceWords as $sentenceWord) {
+            foreach (self::THEMATIC_TAGS as $tag => $thematics ) {
+                foreach ($thematics as $thematicWord) {
+                    if ((strcasecmp($sentenceWord, $thematicWord) == 0)) {
+                        $tags[] = $tag;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+        sort($tags);
+        foreach ($tags as $tag) {
+            $result[] = $tag;
+        }
+        return $result;
+    }
 }
