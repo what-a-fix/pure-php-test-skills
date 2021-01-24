b8661927e354b33468b3f18226d9940fdad1c325
JeanneSic <siche.jeanne@gmail.com>
2021-01-24 17:59:15 +0100

    adding unit tests - simpleTests - worked and failed okay

--- Tests/TextTaggerTest.php
+++ Tests/TextTaggerTest.php
@@ -1,7 +1,42 @@
 <?php
 
+namespace Whatafix\TextTagger\Test;
 
-class TextTaggerTest
+use PHPUnit\Framework\TestCase;
+use Whatafix\TextTagger\TextTagger;
+
+final class TextTaggerTest extends TestCase
 {
+    public function testEmpty(): void
+    {
+        $input = "";
+        $textTagger = new TextTagger();
+        $tag = $textTagger->getTags($input);
+        $this->assertEquals([], $tag);
+    }
+
+    public function testSimple(): void
+    {
+        $input = "I love how my kitchen is decorated. My dog loves it too.";
+        $textTagger = new TextTagger();
+        $tag = $textTagger->getTags($input);
+        $this->assertEquals([
+            'animal',
+            'house',
+        ], $tag);
+    }
+
+    public function testSimple2(): void
+    {
+        $input = "I need a new television in my living-room";
+        $textTagger = new TextTagger();
+        $tag = $textTagger->getTags($input);
+        $this->assertEquals([
+            'house',
+            'technology',
+        ], $tag);
+    }
+
+
 
 }
